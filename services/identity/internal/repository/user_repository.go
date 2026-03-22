39ef5f7af2f62c6191ec5401b1038c9421ce2505
tojinguyen <toainguyenprogramminggame@gmail.com>
2026-03-22 09:17:44 +0700

    feat(identity): update user repositoty using context

--- services/identity/internal/repository/user_repository.go
+++ services/identity/internal/repository/user_repository.go
@@ -1,16 +1,18 @@
 package repository
 
 import (
+	"context"
+
 	"github.com/tojinguyen/identity/internal/domain"
 	"gorm.io/gorm"
 )
 
 type UserRepository interface {
-	Create(user *domain.User) error
-	GetByEmail(email string) (*domain.User, error)
-	GetByID(id string) (*domain.User, error)
-	Update(user *domain.User) error
-	Delete(id string) error
+	Create(ctx context.Context, user *domain.User) error
+	GetByEmail(ctx context.Context, email string) (*domain.User, error)
+	GetByID(ctx context.Context, id string) (*domain.User, error)
+	Update(ctx context.Context, user *domain.User) error
+	Delete(ctx context.Context, id string) error
 }
 
 type userRepository struct {
@@ -21,32 +23,32 @@ func NewUserRepository(db *gorm.DB) UserRepository {
 	return &userRepository{db: db}
 }
 
-func (r *userRepository) Create(user *domain.User) error {
-	return r.db.Create(user).Error
+func (r *userRepository) Create(ctx context.Context, user *domain.User) error {
+	return r.db.WithContext(ctx).Create(user).Error
 }
 
-func (r *userRepository) GetByEmail(email string) (*domain.User, error) {
+func (r *userRepository) GetByEmail(ctx context.Context, email string) (*domain.User, error) {
 	var user domain.User
-	err := r.db.Where("email = ?", email).First(&user).Error
+	err := r.db.WithContext(ctx).Where("email = ?", email).First(&user).Error
 	if err != nil {
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (r *userRepository) GetByID(id string) (*domain.User, error) {
+func (r *userRepository) GetByID(ctx context.Context, id string) (*domain.User, error) {
 	var user domain.User
-	err := r.db.Where("id = ?", id).First(&user).Error
+	err := r.db.WithContext(ctx).Where("id = ?", id).First(&user).Error
 	if err != nil {
 		return nil, err
 	}
 	return &user, nil
 }
 
-func (r *userRepository) Update(user *domain.User) error {
-	return r.db.Save(user).Error
+func (r *userRepository) Update(ctx context.Context, user *domain.User) error {
+	return r.db.WithContext(ctx).Save(user).Error
 }
 
-func (r *userRepository) Delete(id string) error {
-	return r.db.Where("id = ?", id).Delete(&domain.User{}).Error
+func (r *userRepository) Delete(ctx context.Context, id string) error {
+	return r.db.WithContext(ctx).Where("id = ?", id).Delete(&domain.User{}).Error
 }
