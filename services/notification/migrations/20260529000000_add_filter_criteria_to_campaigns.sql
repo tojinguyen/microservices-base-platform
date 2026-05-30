3515a1ca999f58753ae07a18c9b3249ba392e831
Nguyễn Văn Toại <toainguyenprogramminggame@gmail.com>
2026-05-30 15:49:12 +0700

    feat: update cloud dbeaver

--- services/notification/migrations/20260529000000_add_filter_criteria_to_campaigns.sql
+++ services/notification/migrations/20260529000000_add_filter_criteria_to_campaigns.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 ALTER TABLE campaigns
-    ADD COLUMN IF NOT EXISTS filter_criteria JSONB NOT NULL DEFAULT '{}',
-    ALTER COLUMN target_audience SET DEFAULT 'all_users';
+    ADD COLUMN IF NOT EXISTS filter_criteria JSONB       NOT NULL DEFAULT '{}',
+    ADD COLUMN IF NOT EXISTS target_audience VARCHAR(50) NOT NULL DEFAULT 'all_users';
 
 -- +goose Down
 ALTER TABLE campaigns
     DROP COLUMN IF EXISTS filter_criteria,
-    ALTER COLUMN target_audience SET DEFAULT 'specific';
+    DROP COLUMN IF EXISTS target_audience;
